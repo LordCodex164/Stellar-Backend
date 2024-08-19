5ce6b3994921bc8829e4554ea2c7726c60dded25
LordCodex <adenirandaniel565@gmail.com>
2024-08-19 03:33:11 +0100

    small fix

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,7 @@ const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
     res.status(200).json("testing") 
 })
+
 app.use("/api/v1/proposal", proposal)
 app.use("/api/v1/vote", voting)
 app.use("/api/v1/payment",payment)
