b548ca5ffb8cc2e49bc2b3d4ae14a9800e5b71b7
LordCodex <adenirandaniel565@gmail.com>
2024-08-20 14:57:57 +0100

    configure cors remove logs

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -5,12 +5,9 @@ import proposal from '../models/proposal';
 export function scheduleExpiredProposals() {
   cron.schedule('*/5 * * * *', async () => {
     const expiredProposals = await proposal.find({ status: 'active', deadline: { $lt: new Date() } });
-    console.log(`Found ${expiredProposals.length} expired proposals`);
     for (let proposal of expiredProposals) {
       proposal.status = 'expired';
       await proposal.save();
     }
-
-    console.log(`Updated ${expiredProposals.length} expired proposals`);
   });
 }
